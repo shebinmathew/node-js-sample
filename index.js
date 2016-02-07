dab0d212dde9dfd6d855d12a733a89cf8b7a03de
shebinmathew <mailtoshebin@yahoo.com>
2016-02-07 20:38:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-   var x = JSON.stringify(response)
+   var x = JSON.stringify("Hello World");
   response.send(x)
 })
 
