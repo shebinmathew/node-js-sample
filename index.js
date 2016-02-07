968dfbc78541d8ad23b50eb8596f026293fa4986
shebinmathew <mailtoshebin@yahoo.com>
2016-02-07 20:14:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+  console.log(request)
   response.send('Hello World!')
 })
+
+function goodtimeCheck(){}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
