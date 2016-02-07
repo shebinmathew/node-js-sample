8a95f2a4e7938c3ad788931ea15ab121413b2b86
shebinmathew <mailtoshebin@yahoo.com>
2016-02-07 20:23:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-   
+   console.log('1234')
   response.send('hello')
 })
 
