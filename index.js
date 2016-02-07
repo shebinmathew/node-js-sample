ee78b16f868b4c22d25bfdc3bb2cc88a15f0ce19
shebinmathew <mailtoshebin@yahoo.com>
2016-02-07 22:40:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-var express = require('express')
+ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-var tabel = [{"free":0.5,1:"no",1.5:"yes","2":"yes"},{"free":1,1:"yes",1.5:"yes","2":"yes"},{"free":1.5,1:"no",1.5:"yes","2":"yes"}];
-app.get('/', function(request, response) {
+ app.get('/', function(request, response) {
    var x = request.body;
-  response.send(x)
+ 
+ goodtimeCheck(1,2)
+  
+  response.send( goodtimeCheck(1,2))   
 })
 
 function goodtimeCheck(sbreak,cfree){
    var breakTime =  sbreak > cfree ?sbreak:cfree;
-   var notificationtable = [{"free":0.5,"minbusy":2},{"free":1,"minbusy":4},{"free":1.5,"minbusy":5}]; 
+   var notificationtable = [{"free":0.5,"minbusy":1},{"free":1,"minbusy":1.5},{"free":1.5,"minbusy":2}]; 
    var hasbeenBusy= 3;
    for( i=0;i<table.length;i++){
        if( breakTime<= notificationtable[i].free){
